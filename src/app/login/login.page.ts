e4dd6b88f6a3152540ba85a38061890dc94c1685
polyfreitas <heejinfetus@gmail.com>
2022-05-31 20:28:43 -0400

    navegacao-nw

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit {
+export class LoginPage {
 
-  constructor() { }
+  constructor(public navCtrl: NavController) {
 
-  ngOnInit() {
   }
 
+login(){
+  this.navCtrl.navigateRoot('Tab1Page');
+ }
 }
